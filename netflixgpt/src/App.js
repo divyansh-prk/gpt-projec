16f7b9e30c5ca8447e69af6bb14f040d031d1bfb
Divyansh <divyansh2375@gmail.com>
2023-12-03 08:54:49 +0530

    cra & tailwind

---
+++ netflixgpt/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800" >
+      hello everyone this is divyansh 
+    
+    </div>
+  );
+}
+
+export default App;
