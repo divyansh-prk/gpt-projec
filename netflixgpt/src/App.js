c76fab5aa54063fb5e72f92ae473d2e0094f7f9d
Divyansh <divyansh2375@gmail.com>
2023-12-03 10:59:49 +0530

    now next starting ROuting

--- netflixgpt/src/App.js
+++ netflixgpt/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Body from './components/Body'
+
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800" >
-      hello everyone this is divyansh 
-    
+    <div >
+    <Body />  
     </div>
   );
 }
